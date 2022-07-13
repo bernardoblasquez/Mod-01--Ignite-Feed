801df6ad132f980a6183a93959b800418ee1f59e
Bernardo <bernardo.bt@gmail.com>
2022-07-13 09:31:05 -0300

    style: criação, importação e estilização da Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
 import {Post} from './Post'
 import './global.css'
+import { Header } from './components/Header'
 
 export function App() {
  
   return (
-    <Post />
+    <>
+      <Header />
+      <Post />
+    </>
+    
   )
 }
