e1b33247424a41d68ace955a294fce6507432486
Bernardo <bernardo.bt@gmail.com>
2022-07-25 08:01:19 -0300

    feat: criação da estrutura principal do site: sidebar e main

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import { useState } from 'react'
 import {Post} from './Post'
+
+import styles from './app.module.css'
 import './global.css'
 import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
 
 export function App() {
  
   return (
-    <>
+    <div>
       <Header />
-      <Post />
-    </>
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post />
+          <Post />
+        </main>
+      </div>
+      
+    </div>
     
   )
 }
