8bca056330cb53b1ba306c6f30ab3582da65099b
Bernardo <bernardo.bt@gmail.com>
2022-08-22 09:06:18 -0300

    refactor: alteração no código para uso do componente Avatar. Eliminação de estilos redundantes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar';
 import styles from './Sidebar.module.css';
 
 export function Sidebar(){
@@ -9,10 +10,7 @@ export function Sidebar(){
             src="https://images.unsplash.com/photo-1607705703571-c5a8695f18f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40" alt="" 
          />
          <div className={styles.profile}>
-            <img 
-               className={styles.avatar}
-               src="https://avatars.githubusercontent.com/u/4926167?v=4" 
-            />
+            <Avatar src="https://avatars.githubusercontent.com/u/4926167?v=4"/>
             <strong>Bernardo Blasquez</strong>
             <span>Front-end-developer</span>
          </div>
